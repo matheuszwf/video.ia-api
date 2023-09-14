0eaa84cad7f4695ac1f4470fe316d2c755bd22b2
matheuszwf <matheuszwf@gmail.com>
2023-09-14 11:16:59 -0300

    feat: add /prompts route

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import { fastify } from "fastify";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
 
 const app = fastify()
 
-app.get("/", () => {
-    return ""
-})
+app.register(getAllPromptsRoute)
 
 app.listen({
-    port: 3333
+  port: 3333
 }).then(()=>{
-    console.log("Server is running on 'http://localhost:3333'")
+  console.log("Server is running on 'http://localhost:3333'")
 })
