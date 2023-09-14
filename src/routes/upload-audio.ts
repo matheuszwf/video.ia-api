296419f7c82c08e90e142ee553c46eedbdac9a3e
matheuszwf <matheuszwf@gmail.com>
2023-09-14 20:11:01 -0300

    feat: add route to upload audio

---
+++ src/routes/upload-audio.ts
@@ -0,0 +1,54 @@
+import { FastifyInstance } from "fastify";
+import fastifyMultipart from "@fastify/multipart";
+import { randomUUID } from "node:crypto";
+import { pipeline } from "node:stream";
+import { promisify } from "node:util";
+import path from "node:path";
+import fs from "node:fs";
+import { prisma } from "../lib/prisma";
+
+const pipe = promisify(pipeline)
+
+export async function uploadAudio(app: FastifyInstance) {
+  app.register(fastifyMultipart, {
+    limits: {
+      fileSize: 1024 * 1024 * 25, // 25MB
+    }
+  })
+
+  app.post("/audio", async (request, reply) => {
+    const data = await request.file()
+    console.log(data)
+
+    if (!data || data.file.bytesRead < 1024) {
+      return reply.status(400).send({
+        error: "No file uploaded."
+      })
+    }
+
+    const extension = path.extname(data.filename)
+
+    if (extension !== ".mp3") {
+      return reply.status(400).send({
+        error: "Invalid file type, only .mp3 is allowed."
+      })
+    }
+
+    const fileBaseName = path.basename(data.filename, extension)
+    const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`
+    const uploadDestination = path.resolve(__dirname, "../../tmp", fileUploadName)
+
+    await pipe(data.file, fs.createWriteStream(uploadDestination))
+
+    const uploadedFile = await prisma.audio.create({
+      data: {
+        name: fileBaseName,
+        path: uploadDestination
+      }
+    })
+
+    return reply.status(200).send({
+      uploadedFile
+    })
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,23 @@
 import { fastify } from "fastify";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadAudio } from "./routes/upload-audio";
+import { createAudioTranscriptionRoute } from "./routes/create-audio-transcription";
+import { generateIaCompletionRoute } from "./routes/generate-ai-completion";
+import { fastifyCors } from "@fastify/cors";
 
 const app = fastify()
+const routes = [
+  getAllPromptsRoute,
+  uploadAudio
+]
 
-app.register(getAllPromptsRoute)
+routes.forEach((route) => {
+  app.register(route)
+})
+
+app.register(fastifyCors, {
+  origin: "*"
+})
 
 app.listen({
   port: 3333
